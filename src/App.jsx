33fe61171a0bb50bfcd9a10f2119966ca0f0ced5
Artemiy <artemiy.shokin@gmail.com>
2026-04-21 16:51:26 +0200

    finished displaying the colors

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { initialColors } from "./lib/colors";
 import Color from "./Components/Color/Color";
 import "./App.css";
 
+function createColorCreation(colors) {
+  return colors.map((color) => <Color key={color.id} color={color} />);
+}
+
 function App() {
   return (
     <>
       <h1>Theme Creator</h1>
-      // display colors here
+      {createColorCreation(initialColors)}
     </>
   );
 }
